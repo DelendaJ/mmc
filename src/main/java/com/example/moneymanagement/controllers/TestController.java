a7986242b1d837cb855eaee9a883892d7b0ff959
Roberto DeDeus Jr <roberto.p.dedeus@gmail.com>
2019-07-18 18:09:36 -0400

    Fix test controller.

--- src/main/java/com/example/moneymanagement/controllers/TestController.java
+++ src/main/java/com/example/moneymanagement/controllers/TestController.java
@@ -2,6 +2,7 @@ package com.example.moneymanagement.controllers;
 
 import com.example.moneymanagement.entities.TestEntity;
 import com.example.moneymanagement.services.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,11 @@ import java.util.List;
 public class TestController {
 
     private TestService testService;
+
+    @Autowired
+    public TestController(TestService testService) {
+        this.testService = testService;
+    }
 
     @GetMapping("/")
     public List<TestEntity> getTestEntities() {

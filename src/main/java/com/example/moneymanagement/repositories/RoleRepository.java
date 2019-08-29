71a3c835d0739075d97e60df1b09c17ff06285a0
D Joseph <ThePrecious@MacBook-Pro-2.localdomain>
2019-08-29 12:04:54 -0400

    create user roles for login logout

---
+++ src/main/java/com/example/moneymanagement/repositories/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.example.moneymanagement.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository {
+}
--- src/main/java/com/example/moneymanagement/repositories/UserRepository.java
+++ src/main/java/com/example/moneymanagement/repositories/UserRepository.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     User findUserByemail(String email);
+    User findByUsername(String username);
 
 
 }
